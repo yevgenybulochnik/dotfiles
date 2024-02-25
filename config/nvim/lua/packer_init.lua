9d89684e70bb72f53cd0db175ca9e91bb565ed19
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2024-02-25 15:51:58 -0600

    Migrate to lazy.nvim from packer

--- config/nvim/lua/packer_init.lua
+++ config/nvim/lua/packer_init.lua
@@ -1,80 +1,72 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-
+return require('lazy').setup({
   -- Color Schemes
-  use 'navarasu/onedark.nvim'
+  'navarasu/onedark.nvim',
 
   -- UI
-  use {
+  {
     'kyazdani42/nvim-tree.lua', -- File Tree navigator
-    requires = {
+    dependencies = {
       'kyazdani42/nvim-web-devicons',
     },
-  }
-  use 'nvim-lualine/lualine.nvim' -- Status line at bottom
-  use 'romgrk/barbar.nvim' -- Buffer tab bar
-  use {
+  },
+  'nvim-lualine/lualine.nvim', -- Status line at bottom
+  'romgrk/barbar.nvim', -- Buffer tab bar
+  {
     'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
 
   -- Completion
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'hrsh7th/cmp-nvim-lsp'
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'saadparwaiz1/cmp_luasnip',
+  'hrsh7th/cmp-nvim-lsp',
   -- use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'ray-x/lsp_signature.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
+  'ray-x/lsp_signature.nvim',
+  'jose-elias-alvarez/null-ls.nvim',
 
   -- LSP
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  }
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
 
   -- Editing
-  use 'lukas-reineke/indent-blankline.nvim'
-  use {
+  'lukas-reineke/indent-blankline.nvim',
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use 'lewis6991/gitsigns.nvim'
-  use 'terrortylor/nvim-comment'
-  use 'tpope/vim-fugitive'
-  use 'windwp/nvim-autopairs'
-  use {
+    build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  },
+  'lewis6991/gitsigns.nvim',
+  'terrortylor/nvim-comment',
+  'tpope/vim-fugitive',
+  'windwp/nvim-autopairs',
+  {
     'phaazon/hop.nvim',
     branch = 'v2',
-  }
-  use 'mfussenegger/nvim-dap'
-  use 'rcarriga/nvim-dap-ui'
-  use {
+  },
+  'mfussenegger/nvim-dap',
+  'rcarriga/nvim-dap-ui',
+  {
     "kylechui/nvim-surround",
-    tag = "*"
-  }
+    version = "*"
+  },
 
   -- Snippets
-  use 'L3MON4D3/LuaSnip'
-  use 'rafamadriz/friendly-snippets'
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  'L3MON4D3/LuaSnip',
+  'rafamadriz/friendly-snippets',
+})
