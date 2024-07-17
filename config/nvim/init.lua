4f8ef43f89fc262fe66c6535f7e13c10a9aa9ce5
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2024-07-16 20:59:15 -0500

    Mapping ctrl [ to escape

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -190,6 +190,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Map Ctrl [ to Esc vs using Ctrl c
+vim.keymap.set({'n', 'i', 'v'}, '<C-[>', '<Esc>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
