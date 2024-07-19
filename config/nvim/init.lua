d0a0a43ea9dc6fa66151770330f08788bb15596d
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2024-07-19 07:48:19 -0500

    Adjust telescope settings to close windows on esc

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -352,11 +352,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<esc>'] = require('telescope.actions').close}
+          },
+        },
         pickers = {
           find_files = {
             find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
