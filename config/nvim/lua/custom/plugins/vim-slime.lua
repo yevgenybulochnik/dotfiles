31b3625cf79d7358ff127456550632dfa5ebf7ef
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2024-08-19 07:52:53 -0500

    Initial vime slime setup, no keybindings

---
+++ config/nvim/lua/custom/plugins/vim-slime.lua
@@ -0,0 +1,14 @@
+return {
+    "jpalardy/vim-slime",
+    init = function()
+        vim.g.slime_no_mappings = 1
+        vim.g.slime_target = 'tmux'
+        vim.g.slime_bracketed_paste = 1
+        vim.g.slime_no_mappings = 1
+        vim.g.slime_dont_ask_default = 1
+        vim.g.slime_default_config = {
+            socket_name = "default",
+            target_pane = "{last}"
+        }
+    end,
+}
