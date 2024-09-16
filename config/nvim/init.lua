2b264037dc7f9944403d4511710026433c35c4d7
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2024-09-15 19:25:13 -0500

    Setup nvim-lint, add sqlfluff format and lint settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -661,6 +661,7 @@ require('lazy').setup({
       -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        sql = {'sqlfluff'},
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -884,7 +885,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
