9941667edb369337b65a8be33096cf0513eadcfe
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2025-04-20 19:42:12 -0500

    Setup initial keymaps and options

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+-- Show line numbers
+vim.opt.number = true
+
+vim.opt.cursorline = true
+
+vim.opt.signcolumn = 'yes'
