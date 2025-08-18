0bedaac85e9af70742dc19657cb2406989c4b820
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2025-08-18 12:44:10 -0500

    Formatting and add conceal level

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,10 +4,11 @@ vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
 vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.opt.cursorline = true
 
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -20,17 +21,19 @@ vim.opt.smartcase = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.opt.conceallevel = 1
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
