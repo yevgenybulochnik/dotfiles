300ed0ce7b356c9aaf9597ca8be92a40d1e89bcd
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2025-08-21 10:00:32 -0500

    Add markdown snippets for code blocks
    This setup uses a loop and a snippet config to create code bocks within
    markdown. This is a good example of using fmta to insert variables and
    nodes.

---
+++ nvim/snippets/markdown.lua
@@ -0,0 +1,37 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+local markdown_cell_types = {
+  { trig = "mcp", celltype = "python" },
+  { trig = "mcsql", celltype = "sql" },
+  { trig = "mcbash", celltype = "bash" },
+}
+
+local snippets_table = {}
+
+for _, v in ipairs(markdown_cell_types) do
+  table.insert(
+    snippets_table,
+    s(
+      { trig = v.trig, snippetType = "autosnippet" },
+      fmta(
+        [[
+          ```<>
+          <>
+          ```
+        ]],
+        { v.celltype, i(0) }
+      )
+    )
+  )
+end
+
+return snippets_table
