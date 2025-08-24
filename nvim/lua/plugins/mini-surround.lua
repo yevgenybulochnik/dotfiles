8d8594bc7f14d386347f4dec165e6b829b61ec50
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2025-08-24 11:11:44 -0500

    Adjust mini.surround keybindings so the dont conflict with flash

--- nvim/lua/plugins/mini-surround.lua
+++ nvim/lua/plugins/mini-surround.lua
@@ -1,5 +1,18 @@
 return {
   "echasnovski/mini.surround",
   version = "*",
-  opts = {},
+  opts = {
+    mappings = {
+      add = "as", -- Add surrounding in Normal and Visual modes
+      delete = "ds", -- Delete surrounding
+      find = "ysf", -- Find surrounding (to the right)
+      find_left = "ysF", -- Find surrounding (to the left)
+      highlight = "hs", -- Highlight surrounding
+      replace = "cs", -- Replace surrounding
+      update_n_lines = "dsn", -- Update `n_lines`
+
+      suffix_last = "dl", -- Suffix to search with "prev" method
+      suffix_next = "dn", -- Suffix to search with "next" method
+    },
+  },
 }
